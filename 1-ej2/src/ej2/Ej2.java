05c5de670becd058e4d24ab8440db041fc96dfbf
Gonzalo <34586600+GonzaBena@users.noreply.github.com>
2018-04-30 16:44:26 -0300

    Ejercicio 2 Guia 1
    Crear un arreglo de N elementos enteros. Llenar el arreglo con N números enteros provistos por el usuario. Escribir una función que tome como parámetro el arreglo y devuelva la posición del elemento más grande del arreglo. Imprimir por pantalla el mayor de los elementos del arreglo.

---
+++ 1-ej2/src/ej2/Ej2.java
@@ -0,0 +1,36 @@
+/*
+Crear un arreglo de N elementos enteros. Llenar el arreglo con N números enteros provistos por el usuario. 
+Escribir una función que tome como parámetro el arreglo y devuelva la posición del elemento más grande del arreglo. 
+Imprimir por pantalla el mayor de los elementos del arreglo.
+ */
+package ej2;
+import java.util.Scanner;
+public class Ej2 {
+    public static void main(String[] args)
+    {
+        int n = 0;
+        Scanner entry = new Scanner(System.in);
+        System.out.print("Ingrese el tamano del Array: ");
+        n = entry.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length ;i++)
+        {
+            System.out.print("Ingrese los valores del Array: ");
+            array[i] = entry.nextInt();
+        }
+        System.out.println("El menor numero es: "+Ej2.menor(array));
+    }
+
+    public static int menor(int[] array)
+    {
+        int menor = array[0];
+        for(int i = 1; i < array.length;i++)
+        {
+            if (menor < array[i])
+            {
+                menor = array[i];
+            }
+        }
+        return menor;
+    }
+}
